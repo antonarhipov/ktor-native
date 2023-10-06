c7186ac19a41560f7ab13c644ca9f2254ddc5dd3
anton <ant.arhipov@gmail.com>
2023-10-06 20:37:17 +0300

    Initial commit

---
+++ src/commonMain/kotlin/Application.kt
@@ -0,0 +1,13 @@
+import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
