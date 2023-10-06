8f3c13de18e61a2f2995bc8e1723512d03b3753d
anton <ant.arhipov@gmail.com>
2023-10-06 22:53:20 +0300

    Gradle 7.5.1 -> 7.6.3. Jib Serialization/JSON arrow-kt SuspendApp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,109 @@
 val ktor_version: String by project
 val kotlin_version: String by project
-val logback_version: String by project
 
-plugins {
-    kotlin("jvm") version "1.9.10"
-    id("io.ktor.plugin") version "2.3.5"
+buildscript {
+    dependencies {
+        classpath("com.google.cloud.tools:jib-native-image-extension-gradle:0.1.0")
+    }
 }
 
-group = "com.example"
-version = "0.0.1"
-
-application {
-    mainClass.set("com.example.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+plugins {
+    application
+    kotlin("multiplatform") version "1.9.10"
+    id("com.google.cloud.tools.jib") version ("3.4.0")
 }
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
-dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-cio-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+kotlin {
+    //region original
+    val hostOs = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && arch == "x86_64" -> macosX64("native")
+        hostOs == "Mac OS X" && arch == "aarch64" -> macosArm64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        // Other supported targets are listed here: https://ktor.io/docs/native-server.html#targets
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    //endregion
+
+    linuxX64 {
+        binaries {
+            executable(listOf(DEBUG, RELEASE)) {
+                entryPoint = "main"
+                linkerOpts("--as-needed")
+                freeCompilerArgs += "-Xoverride-konan-properties=linkerGccFlags.linux_x64=-lgcc -lgcc_eh -lc"
+            }
+        }
+    }
+
+    macosX64 {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+
+    //region macOS Arm64
+    macosArm64 {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    //endregion
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-core:$ktor_version")
+                implementation("io.ktor:ktor-server-cio:$ktor_version")
+                implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+                implementation("io.arrow-kt:suspendapp:0.4.0")
+                implementation("io.arrow-kt:suspendapp-ktor:0.4.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.ktor:ktor-server-test-host:$ktor_version")
+            }
+        }
+    }
 }
+
+tasks.register<Copy>("copyBinary") {
+    dependsOn(tasks.first { it.name.contains("linkReleaseExecutable") })
+    from(layout.buildDirectory.file("bin/linuxX64/releaseExecutable/ktor-native.kexe"))
+//    into(layout.buildDirectory.dir("native/nativeCompile"))
+    into(layout.buildDirectory.dir("app"))
+}
+
+tasks.withType<com.google.cloud.tools.jib.gradle.JibTask> {
+    dependsOn("copyBinary")
+}
+
+jib {
+    from {
+        image = "gcr.io/distroless/base"
+//        image = "alpine"
+    }
+    pluginExtensions {
+        pluginExtension {
+            implementation = "com.google.cloud.tools.jib.gradle.extension.nativeimage.JibNativeImageExtension"
+            properties = mapOf(Pair("imageName", "ktor-native.kexe"))
+        }
+    }
+    container {
+        mainClass = "ApplicationKt"
+    }
+}
+
+//sourceSets.create("main")
--- src/commonMain/kotlin/Application.kt
+++ src/commonMain/kotlin/Application.kt
@@ -1,13 +1,47 @@
-import com.example.plugins.*
+//import com.example.plugins.*
+//import io.ktor.server.application.*
+//import io.ktor.server.cio.*
+//import io.ktor.server.engine.*
+//
+//fun main() {
+//    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+//        .start(wait = true)
+//}
+//
+//fun Application.module() {
+//    configureRouting()
+//}
+
+
+import arrow.continuations.SuspendApp
+import arrow.continuations.ktor.server
+import arrow.fx.coroutines.resourceScope
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
-import io.ktor.server.engine.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.serialization.Serializable
 
-fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
 
-fun Application.module() {
-    configureRouting()
+@Serializable
+data class Data(val id: Int, val title: String, val description: String)
+
+fun main() = SuspendApp {
+    resourceScope {
+        server(CIO, port = 8080) {
+            install(ContentNegotiation) {
+                json()
+            }
+
+            routing {
+                get("/") {
+                    call.respond(Data(123, "Hello", "This is the response"))
+                }
+            }
+        }
+        awaitCancellation()
+    }
 }
